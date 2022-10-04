5769fc9fca6667832819b53b029272344bd780b3
Dmither <mr.d.graumann@gmail.com>
2022-10-04 15:09:23 +0300

    Create Header

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.header__burger').click(function(event){
+    $('.header__burger').toggleClass('header__burger_active')
+    $('.menu-mobile').toggleClass('menu-mobile_active')
+    $('.wrapper').toggleClass('lock');
+  });
+});

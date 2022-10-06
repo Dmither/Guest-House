e53b55850d211393cbeb7e9664759b3934d1f146
Dmither <mr.d.graumann@gmail.com>
2022-10-06 11:15:36 +0300

    add paralax for intro

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 $(document).ready(function(){
   $('.header__burger').click(function(event){
     $('.header__burger').toggleClass('header__burger_active')
@@ -6,3 +8,30 @@ $(document).ready(function(){
     $('body').toggleClass('lock');
   });
 });
+
+window.onload = function(){
+  const parallax = document.querySelector('.intro');
+  if (parallax) {
+    const bg = document.querySelector('.intro__background');
+    console.log(bg);
+    const speed = 0.05;
+
+    let positionX = 0;
+    let coordXpercent = 0;
+
+    function setMouseParallax(){
+      const distX = coordXpercent - positionX;
+
+      positionX += (distX*speed);
+      bg.style.cssText = `transform: translate(${-positionX / 20}%);`;
+      requestAnimationFrame(setMouseParallax);
+    }
+    setMouseParallax();
+
+    parallax.addEventListener("mousemove", function(e){
+      const parallaxWidth = parallax.offsetWidth;
+      const coordX = e.pageX - parallaxWidth / 2;
+      coordXpercent = coordX / parallaxWidth * 100;
+    });
+  }
+}
